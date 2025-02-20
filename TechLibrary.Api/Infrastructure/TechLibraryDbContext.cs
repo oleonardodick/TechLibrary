6fb1ccaa5e09fa2b5be1581d8605b58f0f0c0ba9
Leonardo Bernardes <leonardod.bernardes@gmail.com>
2025-02-20 11:18:33 -0300

    Add project files.

---
+++ TechLibrary.Api/Infrastructure/TechLibraryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechLibrary.Api.Domain.Entities;
+
+namespace TechLibrary.Api.Infrastructure
+{
+    public class TechLibraryDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\User\\Documents\\Desenvolvimento\\TechLibrary\\TechLibraryDb.db");
+
+        }
+    }
+}
---
+++ TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -0,0 +1,44 @@
+﻿using TechLibrary.Api.Domain.Entities;
+using TechLibrary.Api.Infrastructure;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+using TechLibrary.Exception;
+
+namespace TechLibrary.Api.UseCases.Users.Register
+{
+    public class RegisterUserUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestUserJson request)
+        {
+            Validate(request);
+
+            var entity = new User
+            {
+                Name = request.Name,
+                Email = request.Email,
+                Password = request.Password,
+            };
+
+            var dbContext = new TechLibraryDbContext();
+            dbContext.Users.Add(entity);
+            dbContext.SaveChanges();
+
+            return new ResponseRegisteredUserJson 
+            {
+                Name = entity.Name,
+            };
+        }
+
+        private void Validate(RequestUserJson request)
+        {
+            var validator = new RegisterUserValidator();
+            var result = validator.Validate(request);
+
+            if (result.IsValid == false)
+            {
+                var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+                throw new ErrorOnValidationException(errorMessages);
+            }
+        }
+    }
+}
